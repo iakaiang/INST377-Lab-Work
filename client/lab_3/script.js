4a9123f47f93cdd604c0e5292b9ce7a6c84aea08
iakaiang <kkaiyuan@hotmail.com>
2022-09-23 12:15:23 -0400

    fin

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -49,6 +49,12 @@ function moveToNextSlide() {
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition -= 1;
+  }
+  updateSlidePosition();
   // add your code in here for when you click the "prev" button
   /*
     add an if statement here that checks
